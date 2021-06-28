040b1e822e1ea3eb210781f66a3410532a537572
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2021-06-28 18:22:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -102,5 +102,27 @@ client.on("message", async message => {
     message.reply(`an error occurred while running the command: ${err.message}`);
   }
 });
+const express = require("express");
+const app = express();
+
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/dreams", (request, response) => {
+  response.json(dreams);
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
 
 client.login(token);
